fa678025f2ad2121fbb98e8c1fbee3ea11016050
Steven Stackhouse <stackmm@gmail.com>
2023-04-14 09:39:42 -0600

    add add_race tests

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -12,5 +12,17 @@ RSpec.describe Election do
   it "has attributes" do
     election1 = Election.new("2024")
     expect(election1.year).to eq("2024")
+    expect(election1.races).to eq([])
+  end
+
+  it "can add races to an election" do
+    election1 = Election.new("2024")
+    expect(election1.races).to eq([])
+
+    race1 = Race.new("Presidency")
+    race2 = Race.new("Senator")
+    election1.add_race(race1)
+    election1.add_race(race2)
+    expect(election1.races).to eq([race1, race2])
   end
 end
