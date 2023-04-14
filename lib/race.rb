c49a11f33d5811294cdb1bea64ebededcd836d0c
Steven Stackhouse <stackmm@gmail.com>
2023-04-14 10:07:51 -0600

    add #open? and #close! methods

--- lib/race.rb
+++ lib/race.rb
@@ -4,12 +4,21 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @is_open = true
   end
 
   def register_candidate!(attributes)
     candidate = Candidate.new(attributes)
     @candidates << candidate
     candidate
+  end
+
+  def open?
+    @is_open
+  end
+
+  def close!
+    @is_open = false
   end
 
 end
