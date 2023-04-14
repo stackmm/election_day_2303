d3eda0cb5c3d040a5b97a5ed2d6686f220f5eca4
Steven Stackhouse <stackmm@gmail.com>
2023-04-14 09:39:31 -0600

    add add_race method

--- lib/election.rb
+++ lib/election.rb
@@ -1,8 +1,13 @@
 class Election 
-  attr_reader :year
+  attr_reader :year, :races
 
   def initialize(year)
     @year = year
+    @races = []
   end
-  
+
+  def add_race(race)
+    @races << race
+  end
+
 end
