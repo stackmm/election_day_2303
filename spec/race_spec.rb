bb28cc2e4bd5078a0644b3cfc3da79ff72f87e80
Steven Stackhouse <stackmm@gmail.com>
2023-04-14 09:29:19 -0600

    add register_candidate! tests

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -11,5 +11,20 @@ RSpec.describe Race do
   it "has attributes" do
     race = Race.new("Texas Governor")
     expect(race.office).to eq("Texas Governor")
+    expect(race.candidates).to eq([])
+  end
+
+  it "can register a candidate" do
+    race = Race.new("Texas Governor")
+    expect(race.candidates).to eq([])
+
+    candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+    expect(candidate1.class).to eq(Candidate)
+    expect(candidate1.name).to eq("Diana D")
+    expect(candidate1.party).to eq(:democrat)
+    expect(race.candidates).to eq([candidate1])
+
+    candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+    expect(race.candidates).to eq([candidate1, candidate2])
   end
 end
